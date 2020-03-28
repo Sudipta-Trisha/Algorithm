e33348cf538081c2564b4f30a25035010b7d769c
Sudipta Banik Trisha <43735253+Sudipta-Trisha@users.noreply.github.com>
2020-03-28 12:26:49 +0600

    Update prime factorization.cpp

--- prime factorization.cpp
+++ prime factorization.cpp
@@ -1,33 +1,54 @@
 #include<bits/stdc++.h>
 #define ll long long int
 using namespace std;
+#define sz 10000000
 
-void factorization(ll num)
+vector<ll>v;
+bool arr[10000000+10];
+
+void factorization (ll n)
 {
-	while(num%2==0){
-		cout<<"2"<<" ";
-		num/=2;
-	}
+  
+  ll fact = n;
+  
+  for(ll i=0; i<v.size(); i++){
+    ll x = v[i];
+    if(x*x>n) 
+      break;
+    while(n%x==0){
+      cout<<x<<" ";
+      n/=x;
+    }
+  }
+  if(n>1){
+    cout<<n<<" ";
+  }
+  cout<<endl;
+}
 
-	for(ll i=3; i<=sqrt(num); i+=2){
-		if(num==1)
-			break;
-		while(num%i==0){
-			cout<<i<<" ";
-			num /= i;
-		}
-	}
-
-	if(num>2)
-		cout<<num<<endl;
+void sieve()
+{
+  v.push_back(2);
+  for(ll i=4; i<=sz; i+=2 )
+    arr[i]=1;
+  for(ll i=3; i<=sz; i+=2){
+      if(!arr[i]){
+        v.push_back(i);
+        if(i*i<=sz){
+          for(ll j=i*i; j<=sz; j+=i+i){
+            arr[j]=1;
+          }
+        }
+      }
+      
+    }
 }
 
 int main()
 {
-	ll num;
-	cout<<"Enter a positive number: ";
-	cin>>num;
-
-	factorization(num);
-	return 0;
+  sieve();
+  ll n;
+  cin>>n;
+  factorization(n);
+  return 0;
 }
