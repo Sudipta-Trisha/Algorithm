9501dcdce75efcb59030704be85f4de8f0b7da5b
Sudipta Banik Trisha <43735253+Sudipta-Trisha@users.noreply.github.com>
2020-03-28 12:01:11 +0600

    Update sieve.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -1,35 +1,38 @@
 #include<bits/stdc++.h>
 #define ll long long int
 using namespace std;
+#define sz 10000000
 
+vector<ll>v;
+bool arr[10000000+10];
 void sieve()
 {
-    ll n;
-    //cout<<"Enter Range: ";
-    cin>>n;
-    bool arr[100000+30] = {};
-    
-    for(int i=2; i<=sqrt(n); i++){
-		if(arr[i]==0)
-        for(int j=i; i*j<=n; j++)
-        {
-            if(arr[i*j]==0)
-                arr[i*j] = 1;
-                
+  v.push_back(2);
+  for(ll i=4; i<=sz; i+=2 )
+    arr[i]=1;
+  for(ll i=3; i<=sz; i+=2){
+      if(!arr[i]){
+        v.push_back(i);
+        if(i*i<=sz){
+          for(ll j=i*i; j<=sz; j+=i+i){
+            arr[j]=1;
+          }
         }
+      }
+      
     }
-    for(int i=2; i<=n; i++)
-    {
-        if(arr[i]==0)
-        {
-            cout<<i<<" ";
-        }
-    }
-    cout<<endl;
-    
+  
+  cout<<"Prime numbers are:"<<endl;
+  for(ll i =0; i<v.size(); i++)
+  {
+    cout<<v[i]<<" ";
+  }
+  
+  cout<<endl;
 }
+
 int main()
 {
-    sieve();
-    return 0;
+  sieve();
+  return 0;
 }
