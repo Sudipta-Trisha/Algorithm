6c25a7be722c03924265675a0893aa3be6101270
Sudipta Banik Trisha <sudiptatrisha.19@gmail.com>
2020-04-30 08:54:55 -0700

    Number Theory

--- Extended GCD.cpp
+++ Extended GCD.cpp
@@ -1,28 +1,30 @@
-//Extended GCD
+//Extended GCD//
+
 #include<bits/stdc++.h>
+#define  ll long long int
 using namespace std;
 
-int egcd(int a, int b, int *x, int *y)
+ll egcd(ll a, ll b, ll& x, ll& y) // here &x, &y stands for call by reference which means, if the value of x y change inside of egcd function , then the value of x & y also change inside of main function// 
 {
-    if(a==0){
-        *x = 0;
-        *y = 1;
-        return b;
+    if(b==0){
+        x = 1;
+        y = 0;
+        return a;
     }
-    int x1, y1;
-    int gcd = egcd(b%a,a,&x1,&y1);
-    
-    *x = y1 - (b/a)*x1;
-    *y = x1;
-    
-    return gcd;
+    ll x1, y1;
+    ll g = egcd(b,a%b,x1,y1);
+
+    x = y1;
+    y = x1 - y1*(a/b);
+
+    return g;
 }
 
 int main()
 {
-    int a,b,x,y;
+    ll a,b,x,y;
     cin>>a>>b;
-    int ans = egcd(a,b,&x,&y);
+    ll ans = egcd(a,b,x,y);
     cout<<ans<<endl;
     return 0;
 }
